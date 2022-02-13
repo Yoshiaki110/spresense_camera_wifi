c0ffe1a007f3a43be7702f2d0ca5d123d6c8653d
yoshiaki110 <yoshiaki110@gmail.com>
2022-02-13 19:12:03 +0900

    MEME連携

--- tcp_server.js
+++ tcp_server.js
@@ -1,68 +1,24 @@
-require('date-utils');
-
 var net = require('net');
 var port = 10001;
-var server;
-
-var dt = new Date();
-var formatted = dt.toFormat("HH24:MI:SS");
-var startTime=0;
-var endTime;
+var tcp_server;
 var len=0;
 
 var fs = require('fs');
-var fname = "TCP_TEST"
-var wdata;
 var _conn;
 
 
-// File Write Function
-function writeFile(path, data) {
-    fs.appendFile(path, data, function (err) {
-	if (err) {
-            throw err;
-	}
-    });
-}
+tcp_server = net.createServer(function(conn){
+  _conn = conn;
+  console.log('server-> tcp server created');
 
+  conn.on('data', function(data){
+    len = len + data.length;
+  });
 
-server = net.createServer(function(conn){
-    _conn = conn;
-    console.log('server-> tcp server created');
-
-    conn.on('data', function(data){
-	endTime = new Date();
-	len = len + data.length;
-	
-	if( endTime - startTime > 1000 ){
-	    dt = new Date();
-	    formatted = dt.toFormat("HH24:MI:SS");
-	    console.log(formatted, Math.floor(len*8/(endTime-startTime)), "Kbps");
-	    wdata = formatted + "," + Math.floor(len*8/(endTime-startTime)) + "Kbps\r\n";
-	    writeFile(fname, wdata);
-	    len = 0;
-	    startTime = endTime;
-            conn.write('aaa');
-	}
-    });
-	
-    conn.on('close', function(){
-	console.log('server-> client closed connection');
-    });
-
-    
+  conn.on('close', function(){
+    console.log('server-> client closed connection');
+  });
 }).listen(port);
-
-
-if( process.argv.length > 2 ){
-    fname = fname + "_" + process.argv[2] + ".log";
-}
-else{
-    console.log('usage: node tcp_server.js <log file name>');
-    testtime = new Date();
-    formatted = testtime.toFormat("HH24MISS");
-    fname = fname + "_" + formatted + ".log";
-}
 
 console.log('listening on port %d', port);
 
@@ -74,12 +30,11 @@ const readline = require('readline');
  */
 const main = async () => {
   for (;;) {
-    console.log('エリクサーちょうだい！');
-    if (await confirm('> あげますか？')) {
-      console.log('ありがとう！^_^');
+    if (await confirm('撮影しますか？>')) {
+      console.log('シャッター切りました');
       _conn.write('aaa');
     } else {
-      console.log('死ね！');
+      console.log('なにもしません');
     }
     console.log('');  // 改行
   }
@@ -109,8 +64,78 @@ const question = (question) => {
   });
 };
 
+
+const server = require("ws").Server;
+const ws_server = new server({ port: 8080 });
+var blinks = [];
+var eyeMoves = [];
+var noises = [];
+
+ws_server.on("connection", ws => {
+  console.log("connected from client");
+  ws.on('message',function(message){
+    if(message.indexOf("heartbeat") === -1){
+      //メッセージを出力
+      //console.log(message);
+
+      //実際の処理する場合はparseして処理していきます
+      const obj = JSON.parse(message);
+      //console.log(obj);
+      //console.log(obj['sequenceNumber']);
+      //console.log(obj['eyeMoveUp'], obj['eyeMoveDown'], obj['eyeMoveRight'], obj['eyeMoveLeft']);
+      //console.log(obj['blinkSpeed'], obj['blinkStrength']);
+      //console.log(obj['fitError'], obj['noiseStatus']);
+      var blink = (obj['blinkSpeed'] != 0 || obj['blinkStrength'] != 0) ? 1 : 0;
+      blinks.push(blink);
+      if (blinks.length > 20) {
+        blinks.shift();
+      }
+      var eyeMove = (obj['eyeMoveUp'] != 0 || obj['eyeMoveDown'] != 0 || obj['eyeMoveRight'] != 0 || obj['eyeMoveLeft'] != 0) ? 1 : 0;
+      eyeMoves.push(eyeMove);
+      if (eyeMoves.length > 20) {
+        eyeMoves.shift();
+      }
+      var noise = obj['noiseStatus'] ? 1 : 0;
+      noises.push(noise);
+      if (noises.length > 40) {
+        noises.shift();
+      }
+      let bs = blinks.reduce((sum, element) => sum + element, 0);
+      let es = eyeMoves.reduce((sum, element) => sum + element, 0);
+      let ns = noises.reduce((sum, element) => sum + element, 0);
+      let shoot = bs > 2 || es > 3 || ns > 30 ? 'shoot!!' : ''
+      console.log(blinks.length, eyeMoves.length, bs, es, ns, shoot);
+    }
+  });
+});
+
+
+
+
 // 起動
 (async () => {
   await main();
 })();
 
+
+/*
+{
+  sequenceNumber: 215,
+  pitch: 0,
+  eyeMoveRight: 0,
+  yaw: 0,
+  accX: -1.75,
+  blinkSpeed: 0,
+  powerLeft: 4,
+  accY: 13.0625,
+  roll: 0,
+  eyeMoveDown: 0,
+  fitError: 0,
+  eyeMoveUp: 0,
+  noiseStatus: false,
+  accZ: -9.4375,
+  eyeMoveLeft: 0,
+  blinkStrength: 0,
+  walking: false
+}
+*/
